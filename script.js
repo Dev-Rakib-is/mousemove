4fff1c52ad93d880a982044627a5b95e99c79033
Rakibul Islam <ri3390990@gmail.com>
2025-02-23 16:34:12 +0600

    Fixed touchmove event issue on mobile

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-document.querySelectorAll('.box').forEach(function(box){box.addEventListener('mousemove',function(e){
+document.querySelectorAll('.box').forEach(function(box) {
+    function moveEffect(x, y) {
+        document.querySelectorAll('span').forEach(function(span){
+            span.style.left = x + 'px';
+            span.style.top = y + 'px';
+
+        })
+    }
+//Desktop mouse Move
+box.addEventListener('mousemove',function (e) {
     let x = e.pageX - box.offsetLeft;
     let y = e.pageY - box.offsetTop;
+    moveEffect(x, y)
+})
 
-    document.querySelectorAll('span').forEach(function(span){
-        span.style.left = x + 'px';
-        span.style.top = y + 'px';
-    })
-})})
+//Mobile Touch Move
+box.addEventListener('touchmove',function (e) {
+    e.preventDefault();
+
+
+    let Touch = e.touches[0]
+    let x = Touch.pageX - box.offsetLeft;
+    let y = Touch.pageY - box.offsetTop;
+    moveEffect(x, y);
+    console.log("Touch Move: ", x, y);
+})
+})
+
+
+
+
+
